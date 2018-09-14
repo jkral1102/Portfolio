1a8de7ded0b81a4b64b4d9b0be4419682e0a031d
unknown <jkral1101@gmail.com>
2018-09-13 21:50:22 -0500

    push

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
           <Navbar />
 
           <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
+          <Route path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/github" component={Github} />
 
