84aa375acda0b4ca8964bd3e8b828e82fec07eab
unknown <jkral1101@gmail.com>
2018-09-15 17:30:06 -0500

    push

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -18,7 +18,7 @@ class Bio extends Component {
                     <div>
                         <span id='homeTitle'>Hello! </span>
 
-                        <p>I am a Minneapolis-St. Paul based Web Developer. I primarily use the MERN stack for my web applications although I am always striving to incorporate new and improved technologies.</p>
+                        <p>I am a Minneapolis-St. Paul based Web Developer. I develop web applications using the MERN stack (MongoDB, Express, React, and Node.js) although I am always striving to incorporate new and improved technologies.</p>
 
                         <p>I hold true to the principle that things can always be improved. Whether its by using better technologies, refactoring code, or even rethinking an entire App from the ground up.</p>
                     </div>
