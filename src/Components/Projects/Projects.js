e6e755c6d9a307df7851fc1b8a20248a0fa773bb
jkral1102 <jkral1101@gmail.com>
2019-05-09 15:04:15 -0500

    edit project descriptions

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,7 +11,7 @@ class Projects extends Component {
                 {
                     name: 'Memory Game ',
                     img: '/assets/images/memoryGame/logo.png',
-                    info: 'This is a game where you must click all 12 faces without selecting any twice. This was my first app using React!',
+                    info: 'In this game you must click all 12 pictures without selecting any twice. This was my first app built with React!',
                     technology: 'React.js • Javascript',
                     links:
                     {
@@ -24,7 +24,7 @@ class Projects extends Component {
                 {
                     name: 'LunchLotto',
                     img: '/assets/images/lunchlotto/lotto.PNG',
-                    info: 'This app allows you to form a group where members can add restaurant suggestions and it selects a random restaurant. With this app, I learned a variety of fun jQuery animations and Sequelize querying. ',
+                    info: 'Allows you to form a group where members can add restaurant suggestions and selects a random restaurant. With this app, I learned a variety of fun jQuery animations along with Sequelize querying. ',
                     technology: 'Javascript • jQuery • Sequelize • Node.js • Express.js • Postgresql • Google Location API • Yelp API',
                     links:
                     {
@@ -36,9 +36,9 @@ class Projects extends Component {
 
                 },
                 {
-                    name: 'New York Times Search',
+                    name: 'New York Times Forum',
                     img: '/assets/images/nytSearch/nyt.png',
-                    info: 'This  app allows you to save and comment on articles pulled from the New York Times API. This was my first full-stack React application and I learned MongoDB.',
+                    info: 'This forum allows you to save and comment on articles pulled from the New York Times API. This was my first full-stack React application built with MongoDB.',
                     technology: 'React • Javascript • Node.js • Express.js • MongoDB',
                     links:
                     {
@@ -51,7 +51,7 @@ class Projects extends Component {
                 {
                     name: 'City Dashboard',
                     img: '/assets/images/cityDashboard/city.png',
-                    info: 'This app allows you to type in a city name and pull relevant data on weather, news headlines, and local events. This was my first app using Redux and a variety of external APIs in a single application.',
+                    info: 'This dashboard takes in a city name and pulls relevant data on weather, news, and local events. I built this to learn Redux and utilized many external APIs in a single application.',
                     technology: 'Redux • React • Javascript • Node.js • Express.js • OpenWeather API • Bing News API • Eventful API',
                     links:
                     {
@@ -62,9 +62,9 @@ class Projects extends Component {
 
                 },
                 {
-                    name: 'And Lastly...',
+                    name: 'And Lastly... This pPrtfolio!',
                     img: '/assets/images/portfolio.png',
-                    info: 'This Portfolio! I aimed for a simple, mobile-friendly design that is scalable for featuring many projects. Please enjoy.',
+                    info: 'I aimed for a simple, mobile-friendly design that is scalable for use with many projects.',
                     technology: 'React • Javascript',
                     links:
                     {
